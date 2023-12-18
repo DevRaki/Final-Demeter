e95f2066646149966e612ca6913a12cd54aa9173
DevRaki <samuelriosaristizabal@gmail.com>
2023-12-18 07:16:25 -0500

    Update sales.jsx

--- src/Pages/sales.jsx
+++ src/Pages/sales.jsx
@@ -6,154 +6,160 @@ import Bill from './Bill_Sale';
 import Edit_Bill from './EditSale';
 
 function Sales() {
-    const { ProductCategories, fetchProductCategories } = useProductCategories();
-    const [selectedCategories, setSelectedCategories] = useState([]);
-    const [selectedCategory, setSelectedCategory] = useState('');
-    const { fetchProduct, getwholeProducts } = useProduct();
-    const [selectedCategoryName, setSelectedCategoryName] = useState({});
-    const [categoryImages, setCategoryImages] = useState({});
-    const [productNames, setProductNames] = useState({});
-    const { Sale, CreateDetail, getDetailsSale, Count, total, fetchSales, Sales, addnewDetail, newDetails, setNewCost, newCost, action } = useSaleContext();
-    const [newSaleID, setNewSaleID] =useState()
-    useEffect(() => {
-        fetchProductCategories();
-        
-        
-        if (Sales.length > 0 && action == 1) {
-            setNewSaleID((Sales[Sales.length - 1].ID_Sale) + 1);
-        }
-        else if(action == 2){
-            setNewSaleID(Sale.ID_Sale)
+  const { ProductCategories, fetchProductCategories } = useProductCategories();
+  const [selectedCategories, setSelectedCategories] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState('');
+  const { fetchProduct, getwholeProducts } = useProduct();
+  const [selectedCategoryName, setSelectedCategoryName] = useState({});
+  const [categoryImages, setCategoryImages] = useState({});
+  const [productNames, setProductNames] = useState({});
+  const { Sale, CreateDetail, getDetailsSale, Count, total, fetchSales, Sales, addnewDetail, newDetails, setNewCost, newCost, action } = useSaleContext();
+  const [newSaleID, setNewSaleID] = useState();
 
-        }
-        else{
-            setNewSaleID(1);
-        }
-        
-    }, []);
-    const handleCategoryChange = (event) => {
-        const newCategoryID = event.target.value;
+  useEffect(() => {
+    fetchProductCategories();
 
-        if (selectedCategories.includes(newCategoryID)) {
-            const updatedCategories = selectedCategories.filter(
-                (category) => category !== newCategoryID
-            );
-            setSelectedCategories(updatedCategories);
-            const updatedCategoryImages = { ...categoryImages };
-            const updatedProductNames = { ...productNames };
-            delete updatedCategoryImages[newCategoryID];
-            delete updatedProductNames[newCategoryID];
-            setCategoryImages(updatedCategoryImages);
-            setProductNames(updatedProductNames);
-        } else {
-            setSelectedCategories([...selectedCategories, newCategoryID]);
-            setSelectedCategory(newCategoryID);
-            setSelectedCategoryName({ ...selectedCategoryName, [newCategoryID]: event.target.options[event.target.selectedIndex].text });
-            fetchProductsForCategory(newCategoryID);
-        }
-
-        
-    };
-
-    useEffect(() => {
-        if (selectedCategory) {
-            const selectElement = document.getElementById('categorias');
-            selectElement.value = selectedCategory;
-        }
-    }, [selectedCategory]);
-
-    const fetchProductsForCategory = (categoryID) => {
-        fetchProduct(categoryID)
-            .then(data => {
-                console.log(data)
-                const images = data.map(product => product.Image);
-                const names = data.map(product => product.Name_Products);
-                setCategoryImages({ ...categoryImages, [categoryID]: images });
-                setProductNames({ ...productNames, [categoryID]: names });
-            })
-            .catch(error => {
-                console.error('Error al cargar productos:', error);
-            });
-    };
-
-    const detail = ( ID_Product) =>{
-        const data = {
-            Sale_ID : newSaleID,
-            Product_ID : ID_Product,
-            Lot: 1
-        }
-        addnewDetail(data)
-        
+    if (Sales.length > 0 && action === 1) {
+      setNewSaleID(Sales[Sales.length - 1].ID_Sale + 1);
+    } else if (action === 2) {
+      setNewSaleID(Sale.ID_Sale);
+    } else {
+      setNewSaleID(1);
     }
-    const handleImageClick = async (categoryID, imageIndex) => {
-        
-    
-        try {
-            const data = await fetchProduct(categoryID);
-            const selectedProductID = data[imageIndex].ID_Product;
-            detail(selectedProductID);
-        } catch (error) {
-            console.error('Error al cargar productos o detalles:', error);
-        }
-        setSelectedCategory('');
-        console.log(newDetails)
+  }, []);
+
+  const handleCategoryChange = (event) => {
+    const newCategoryID = event.target.value;
+
+    if (newCategoryID === '') {
+      // Reset selected category
+      setSelectedCategories([]);
+      setSelectedCategory('');
+      setSelectedCategoryName({});
+      setCategoryImages({});
+      setProductNames({});
+    } else {
+      if (selectedCategories.includes(newCategoryID)) {
+        const updatedCategories = selectedCategories.filter(
+          (category) => category !== newCategoryID
+        );
+        setSelectedCategories(updatedCategories);
+        const updatedCategoryImages = { ...categoryImages };
+        const updatedProductNames = { ...productNames };
+        delete updatedCategoryImages[newCategoryID];
+        delete updatedProductNames[newCategoryID];
+        setCategoryImages(updatedCategoryImages);
+        setProductNames(updatedProductNames);
+      } else {
+        setSelectedCategories([...selectedCategories, newCategoryID]);
+        setSelectedCategory(newCategoryID);
+        setSelectedCategoryName({
+          ...selectedCategoryName,
+          [newCategoryID]: event.target.options[event.target.selectedIndex].text,
+        });
+        fetchProductsForCategory(newCategoryID);
+      }
+    }
+
+    // Reset selectedCategory to an empty string
+    setSelectedCategory('');
+  };
+
+  useEffect(() => {
+    if (selectedCategory) {
+      const selectElement = document.getElementById('categorias');
+      selectElement.value = selectedCategory;
+    }
+  }, [selectedCategory]);
+
+  const fetchProductsForCategory = (categoryID) => {
+    fetchProduct(categoryID)
+      .then((data) => {
+        const images = data.map((product) => product.Image);
+        const names = data.map((product) => product.Name_Products);
+        setCategoryImages({ ...categoryImages, [categoryID]: images });
+        setProductNames({ ...productNames, [categoryID]: names });
+      })
+      .catch((error) => {
+        console.error('Error al cargar productos:', error);
+      });
+  };
+
+  const detail = (ID_Product) => {
+    const data = {
+      Sale_ID: newSaleID,
+      Product_ID: ID_Product,
+      Lot: 1,
     };
-    
-    
-    return (
-        <div className=" ">
-            <h1 className="text-3xl font-bold mb-4">Ventas 1.0 </h1>
-            <div className='flex flex-grid'>
-            <div className="w-full md:w-[70%] lg:w-[60%] xl:w-[50%] min-h-screen md:min-h-[70%] lg:min-h-[80%] mx-auto md:mx-[25%] lg:mx-[30%] xl:mx-[35%] mr-5">
-                    <div className="float-left mt-[3vh]">
-                        <label htmlFor="categorias" className="text-lg font-medium text-gray-700">Seleccione categorías: </label>
-                        <select
-                            id="categorias"
-                            name="categorias"
-                            className="w-[20vh] h-[1vh]p-2 mt-1 rounded-md border border-orange-300 shadow-sm focus:ring-orange-500 focus:border-orange-500"
-                            value={selectedCategory}
-                            onChange={handleCategoryChange}
-                        >
-                            {ProductCategories.map(category => (
-                                <option key={category.ID_ProductCategory} value={category.ID_ProductCategory}>
-                                    {category.Name_ProductCategory}
-                                </option>
-                            ))}
-                        </select>
-                    </div>
-                    {selectedCategories.map((categoryID, index) => (
-                        <div key={index}>
-                            <h1 className="text-3xl font-bold mb-4 mt-[10vh]">
-                                {selectedCategoryName[categoryID]}
-                            </h1>
-                            <div className="images flex flex-grid">
-                                {categoryImages[categoryID] && categoryImages[categoryID].map((image, imageIndex) => (
-                                    <button
-                                        key={imageIndex}
-                                        className='pl-[3vh] transform transition-transform hover:scale-105'
-                                        onClick={() => handleImageClick(categoryID, imageIndex)}
-                                    >
-                                        <img
-                                            src={image}
-                                            alt=""
-                                            className='h-[3vh] cursor-pointer'
-                                        />
-                                        <p className="text-lg font-semibold text-orange-600">{productNames[categoryID][imageIndex]}</p>
-                                    </button>
-                                ))}
-                            </div>
-                        </div>
-                    ))}
-                </div>
-                <div className="contenedor derecho w-[50vh] bg-gray-200 border border-gray-400 rounded-lg h-full shadow-lg relative mt-[10vh]">
-                    <div className="h-full w-full ">
-                    
-                    {action === 1 ? <Bill /> : action === 2 ? <Edit_Bill /> : null}
-                    </div>
-                </div>
+    addnewDetail(data);
+  };
+
+  const handleImageClick = async (categoryID, imageIndex) => {
+    try {
+      const data = await fetchProduct(categoryID);
+      const selectedProductID = data[imageIndex].ID_Product;
+      detail(selectedProductID);
+    } catch (error) {
+      console.error('Error al cargar productos o detalles:', error);
+    }
+    setSelectedCategory('');
+    console.log(newDetails);
+  };
+
+  return (
+    <div className=" ">
+      <h1 className="text-3xl font-bold mb-4">Ventas 1.0 </h1>
+      <div className="flex flex-grid">
+        <div className="w-full md:w-[70%] lg:w-[60%] xl:w-[50%] min-h-screen md:min-h-[70%] lg:min-h-[80%] mx-auto md:mx-[25%] lg:mx-[30%] xl:mx-[35%] mr-5">
+          <div className="float-left mt-[3vh]">
+            <label htmlFor="categorias" className="text-lg font-medium text-gray-700">
+              Seleccione categorías:
+            </label>
+            <select
+              id="categorias"
+              name="categorias"
+              className="w-[20vh] h-[1vh]p-2 mt-1 rounded-md border border-orange-300 shadow-sm focus:ring-orange-500 focus:border-orange-500"
+              value={selectedCategory}
+              onChange={handleCategoryChange}
+            >
+              {/* Default option */}
+              <option value="" disabled hidden>
+                Seleccione categorías
+              </option>
+              {ProductCategories.map((category) => (
+                <option key={category.ID_ProductCategory} value={category.ID_ProductCategory}>
+                  {category.Name_ProductCategory}
+                </option>
+              ))}
+            </select>
+          </div>
+          {selectedCategories.map((categoryID, index) => (
+            <div key={index}>
+              <h1 className="text-3xl font-bold mb-4 mt-[10vh]">{selectedCategoryName[categoryID]}</h1>
+              <div className="images flex flex-grid">
+                {categoryImages[categoryID] &&
+                  categoryImages[categoryID].map((image, imageIndex) => (
+                    <button
+                      key={imageIndex}
+                      className="pl-[3vh] transform transition-transform hover:scale-105"
+                      onClick={() => handleImageClick(categoryID, imageIndex)}
+                    >
+                      <img src={image} alt="" className="h-[3vh] cursor-pointer" />
+                      <p className="text-lg font-semibold text-orange-600">{productNames[categoryID][imageIndex]}</p>
+                    </button>
+                  ))}
+              </div>
             </div>
+          ))}
         </div>
-    );
+        <div className="contenedor derecho w-[50vh] bg-gray-200 border border-gray-400 rounded-lg h-full shadow-lg relative mt-[10vh]">
+          <div className="h-full w-full ">
+            {action === 1 ? <Bill /> : action === 2 ? <Edit_Bill /> : null}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Sales;
